37edc368f1f3a7138f7c3517d86ef1985ed8c150
Amith Viduranga <62999097+viduranga-bit@users.noreply.github.com>
2022-12-25 18:56:26 +0530

    nurse rest api done

--- src/main/java/com/Server/MediCareServer/config/CustomUserDetailsService.java
+++ src/main/java/com/Server/MediCareServer/config/CustomUserDetailsService.java
@@ -9,6 +9,7 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -24,7 +25,8 @@ public class CustomUserDetailsService implements UserDetailsService {
 	private UserRepository userDao;
 
 	@Autowired
-	private PasswordEncoder bcryptEncoder;
+	private BCryptPasswordEncoder bcryptEncoder;
+	
 	
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
--- src/main/java/com/Server/MediCareServer/repository/DepartmentRepository.java
+++ src/main/java/com/Server/MediCareServer/repository/DepartmentRepository.java
@@ -2,7 +2,6 @@ package com.Server.MediCareServer.repository;
 
 import com.Server.MediCareServer.model.Department;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
