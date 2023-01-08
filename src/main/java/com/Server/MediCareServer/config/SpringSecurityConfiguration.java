4f552f231bfa31c08312f77737c4389d9f05da02
Amith Viduranga <62999097+viduranga-bit@users.noreply.github.com>
2023-01-08 14:40:09 +0530

    patient rest API done

--- src/main/java/com/Server/MediCareServer/config/SpringSecurityConfiguration.java
+++ src/main/java/com/Server/MediCareServer/config/SpringSecurityConfiguration.java
@@ -49,7 +49,7 @@ public class SpringSecurityConfiguration extends WebSecurityConfigurerAdapter {
 				.antMatchers("/hellouser").hasAnyRole("USER", "ADMIN")
 				.antMatchers("/authenticate", "/register", "/validate/{token}", "/api/v1/departments",
 						"/api/v1/departments/{dept_Id}", "/api/v1/users/{username}", "/api/v1/users/role/{role}",
-						"/api/v1/users/delete/{id}")
+						"/api/v1/users/delete/{id}","/api/v1/patients")
 				.permitAll().anyRequest().authenticated()
 				.and().exceptionHandling().authenticationEntryPoint(jwtAuthenticationEntryPoint).and()
 				.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
---
+++ src/main/java/com/Server/MediCareServer/model/Patient.java
@@ -0,0 +1,61 @@
+package com.Server.MediCareServer.model;
+
+import java.sql.Time;
+import java.util.Date;
+
+import javax.persistence.*;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonFormat.Shape;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name="Patient")
+public class Patient {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long patientId;
+    @Column(name = "patient_name")
+    private String patientName;
+    @JsonFormat(pattern = "yyyy-MM-dd",shape = Shape.STRING)
+    @Column(name = "dob")
+    private String dob;
+    @JsonFormat(pattern = "yyyy-MM-dd",shape = Shape.STRING)
+    @Column(name = "admitdate")
+    private String admitdate;
+
+
+    @Temporal(TemporalType.TIME)
+    @Column(name = "admitTime")
+    private Date admitTime=new Date(System.currentTimeMillis());
+
+    @Column(name = "NIC")
+    private String nic;
+
+    @Column(name = "patientType")
+    private String patientType;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "bloodGroup")
+    private String bloodGroup;
+    @Column(name = "address")
+    private String address;
+
+    @Column(name = "city")
+    private String city;
+
+    @Column(name = "mobileNo")
+    private String mobileNo;
+}
